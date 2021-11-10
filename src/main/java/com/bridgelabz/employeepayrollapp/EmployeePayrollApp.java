74fa2b006e250ce9c6aa74a0df20881b9f7ac9b1
Prashanth <imprash4u@gmail.com>
2021-11-10 17:50:29 +0530

    Added:Validation For all Feilds in EmployeePayroll Dto

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApp.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApp.java
@@ -12,10 +12,10 @@ public class EmployeePayrollApp {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(EmployeePayrollApp.class, args);
-       log.info("Employee Payroll App Started in {} Environment",
-               context.getEnvironment().getProperty("environment"));
-       log.info("Employee Payroll DB User is {}",
-               context.getEnvironment().getProperty("spring.datasource.username"));
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
